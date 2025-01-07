5e17685be86bd31c4c7fcd4d23c2dd96daefedfe
Elise MASCRET <elise.mascret@viacesi.fr>
2025-01-07 20:11:02 +0100

    mise en place de la navbar

---
+++ src/View/navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Image from 'next/image'
+
+
+
+export default function Navbar(){
+    return(
+        <>
+        <div className="w-1440px h-114px bg-cyan-100 border border-black flex items-center justify-between">
+            <div>
+                <Image 
+                    src={'/img/logo.png'}
+                    width={150}
+                    height={150}
+                    alt="logo de l'entreprise"
+                />
+            </div>
+            <div className=' border rounded-md'>
+                <input type="Rechercher ..." className='border rounded-md w-52'/>
+            </div>
+            <div>
+                <p>Référentiel de ressources</p>
+            </div>
+            <div>
+                <p>Paramètres</p>
+            </div>
+            <div className='mr-8'>
+                <p>Mon compte</p>
+            </div>
+        </div>
+        <div>Heelllo</div>
+        </>
+    );
+}
